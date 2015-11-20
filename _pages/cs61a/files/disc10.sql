567e97610bab929219f86fb81dcc623db22016b5
Kevin Chen <kvchen@berkeley.edu>
2015-11-20 11:08:21 -0800

    Fix typo in disc10 SQL file

--- _pages/cs61a/files/disc10.sql
+++ _pages/cs61a/files/disc10.sql
@@ -3,7 +3,7 @@ CREATE TABLE records AS
   SELECT "Alyssa P Hacker", "Computer", "Programmer", 40000, "Ben Bitdiddle" UNION
   SELECT "Cy D Fect", "Computer", "Programmer", 35000, "Ben Bitdiddle" UNION
   SELECT "Lem E Tweakit", "Computer", "Technician", 25000, "Ben Bitdiddle" UNION
-  SELECT "Louis ReASoner", "Computer", "Programmer Trainee", 30000, "Alyssa P Hacker" UNION
+  SELECT "Louis Reasoner", "Computer", "Programmer Trainee", 30000, "Alyssa P Hacker" UNION
   SELECT "Oliver Warbucks", "Administration", "Big Wheel", 150000, "Oliver Warbucks" UNION
   SELECT "DeWitt Aull", "Administration", "Secretary", 25000, "Oliver Warbucks" UNION
   SELECT "Eben Scrooge", "Accounting", "Chief Accountant", 75000, "Oliver Warbucks" UNION
